3b13f7b9077c52a6eff24c4b7bad01bdd40ec1e2
Alex <alexpdev@protonmail.com>
2022-08-15 18:58:58 -0700

    updates

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
-let checkstate = true;
+// background.js
 
+let checkstate = true;
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ checkstate });
+  chrome.storage.sync.set({checkstate: checkstate});
   console.log("Checkstate is set");
 })
 
@@ -13,8 +14,7 @@ function handler(details) {
       let transit =  details.transitionQualifiers;
       if (transit.length > 0 && transit[0] == "forward_back"){
         if (details.url.includes("stackoverflow.com")){
-          console.log("This is where I need to refresh the page.");
           chrome.tabs.reload(details.tabId);
     }}} else {
-      console.log("unchecked");
+      console.log(checkstate);
 }})}
--- popup.js
+++ popup.js
@@ -1,28 +1,16 @@
-
-let isActive = document.getElementById("checkActive");
-var state = {}
-
-chrome.storage.sync.get("checkstate", (data) => {
-  isActive.checked = data["checkstate"];
-  state.checkstate = data["checkstate"];
-  console.log(state);
-});
-
 document.addEventListener('DOMContentLoaded', () => {
   let isActive = document.getElementById("checkActive");
+  chrome.storage.sync.get("checkstate", (data) => {
+    isActive.checked = data["checkstate"];
+  });
   isActive.addEventListener("click", checkedHandler);
 })
 
 function checkedHandler() {
-  console.log(state);
   let isActive = document.getElementById("checkActive");
-  if (isActive.checked) {
-    state["checkstate"] = true;
-    chrome.storage.sync.set(state);
-    console.log("isActive is checked");
+  if (isActive.checked === true) {
+    chrome.storage.sync.set({checkstate: isActive.checked});
   } else {
-    state["checkstate"] = false;
-    chrome.storage.sync.set(state);
-    console.log("isActive is unchecked");
+    chrome.storage.sync.set({checkstate: isActive.checked});
   }
 }
