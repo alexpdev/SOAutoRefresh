51f04510190e97fc7a46cf3d729657f9f473f242
Alex <alexpdev@protonmail.com>
2022-09-03 22:46:20 -0700

    updates

--- background.js
+++ background.js
@@ -1,20 +1,22 @@
 // background.js
-
 let checkstate = true;
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({checkstate: checkstate});
-  console.log("Checkstate is set");
-})
-
+  chrome.storage.sync.set({ checkstate: checkstate });
+});
 chrome.webNavigation.onCommitted.addListener(handler);
-
+chrome.runtime.onMessage.addListener((message, port) => {
+  chrome.storage.sync.set({ checkstate: message.checkstate });
+  checkstate = message.checkstate;
+});
 function handler(details) {
-  chrome.storage.sync.get("checkstate", (checkstate) => {
-    if (checkstate){
-      let transit =  details.transitionQualifiers;
-      if (transit.length > 0 && transit[0] == "forward_back"){
-        if (details.url.includes("stackoverflow.com")){
+  chrome.storage.sync.get("checkstate", (state) => {
+    if (state.checkstate === true) {
+      let transit = details.transitionQualifiers;
+      if (transit.length > 0 && transit[0] == "forward_back") {
+        if (details.url.includes("stackoverflow.com")) {
           chrome.tabs.reload(details.tabId);
-    }}} else {
-      console.log(checkstate);
-}})}
+        }
+      }
+    }
+  });
+}
--- popup.js
+++ popup.js
@@ -1,16 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let isActive = document.getElementById("checkActive");
-  chrome.storage.sync.get("checkstate", (data) => {
-    isActive.checked = data["checkstate"];
-  });
-  isActive.addEventListener("click", checkedHandler);
-})
-
-function checkedHandler() {
-  let isActive = document.getElementById("checkActive");
-  if (isActive.checked === true) {
-    chrome.storage.sync.set({checkstate: isActive.checked});
-  } else {
-    chrome.storage.sync.set({checkstate: isActive.checked});
+$('table').tableCheckbox({
+  selectedRowClass:'warning',
+  checkboxSelector:'td:first-of-type input[type="checkbox"],th:first-of-type input[type="checkbox"]',
+  isChecked: function($checkbox) {
+    return $checkbox.is(':checked');
   }
+});
+$('table').tableCheckbox();
+
+document.addEventListener("DOMContentLoaded", () => {
+  let sites = [];
+  let boxes = document.getElementsByClassName("checks");
+  chrome.storage.sync.get("sites", (data) => {
+    sites.concat(data);
+  });
+  for (let i=0; i<boxes.length; i++){
+  
+});
+function checkedHandler() {
+  let sites = [];
+  let boxes = document.getElementsByClassName("checks");
+  for (let i = 0; i < boxes.length; i++){
+    if (boxes[i].checked){
+      sites.push(boxes[i].name);
+      console.log(boxes[i].name);
+    }
+  }
+  chrome.runtime.sendMessage({ sites: sites });
+  chrome.storage.sync.set({ sites: sites });
 }
